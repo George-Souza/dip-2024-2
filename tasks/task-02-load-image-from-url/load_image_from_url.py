7b16b59d4dfe460aa17af8e39d9f836f0378ba5f
George Souza <georgebrandaosouza@gmail.com>
2025-02-22 17:55:13 -0300

    adicionando as atividades 2 e 3

--- tasks/task-02-load-image-from-url/load_image_from_url.py
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import cv2 as cv
+import requests
 
 def load_image_from_url(url, **kwargs):
     """
@@ -15,9 +16,15 @@ def load_image_from_url(url, **kwargs):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    response = requests.get(url)
+    response.raise_for_status()
+
+    image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+
+
+    image = cv.imdecode(image_array, cv.IMREAD_COLOR)
     ### END CODE HERE ###
     
     return image
 
-load_image_from_url()
+load_image_from_url('https://img.elo7.com.br/product/600x380/43AC3C9/miniaturas-de-cachorros-6-cm-2-unid-animal.jpg')
